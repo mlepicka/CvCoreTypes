f6c2e5ad38ab441dabee34510d95c25e54e39795
lzmuda <luka100@onet.eu>
2014-08-07 15:37:35 +0200

    modified:   src/Components/CameraInfoProvider/CameraInfoProvider.cpp 	modified:   src/Components/CameraInfoProvider/CameraInfoProvider.hpp

--- src/Components/CameraInfoProvider/CameraInfoProvider.cpp
+++ src/Components/CameraInfoProvider/CameraInfoProvider.cpp
@@ -1,8 +1,8 @@
 /*!
- * \file
- * \brief
- * \author Maciej Stefańczyk [maciek.slon@gmail.com]
- */
+* \file
+* \brief
+* \author Maciej Stefańczyk [maciek.slon@gmail.com]
+*/
 
 #include <memory>
 #include <string>
@@ -16,27 +16,28 @@ namespace Processors {
 namespace CameraInfoProvider {
 
 CameraInfoProvider::CameraInfoProvider(const std::string & name) :
-		Base::Component(name),
-		width("width", 640),
-		height("height", 480),
-		camera_matrix("camera_matrix", cv::Mat(cv::Mat::eye(3, 3, CV_32FC1))),
+Base::Component(name),
+width("width", 640),
+height("height", 480),
+camera_matrix("camera_matrix", cv::Mat(cv::Mat::eye(3, 3, CV_32FC1))),
         dist_coeffs("dist_coeffs", cv::Mat(cv::Mat::ones(1, 5, CV_32FC1))),
         rectificaton_matrix("rectificaton_matrix",cv::Mat(cv::Mat::eye(3,3,CV_32FC1))),
         projection_matrix("projection_matrix",cv::Mat(cv::Mat::zeros(3,4,CV_32FC1))),
         rotation_matrix("rotation_matrix",cv::Mat(cv::Mat::eye(3,3,CV_32FC1))),
         translation_matrix("translation_matrix",cv::Mat(cv::Mat::zeros(3,1,CV_32FC1))),
-        data_file("data_file", string("~/stereo/camera_info.yml"))
+        //data_file("data_file", string("~/stereo/camera_info.yml"))
+        data_file("data_file", string(""))
 {
-	width.addConstraint("0");
-	width.addConstraint("1280");
-	registerProperty(width);
+width.addConstraint("0");
+width.addConstraint("1280");
+registerProperty(width);
 
-	height.addConstraint("0");
-	height.addConstraint("1280");
-	registerProperty(height);
+height.addConstraint("0");
+height.addConstraint("1280");
+registerProperty(height);
 
-	registerProperty(camera_matrix);
-	registerProperty(dist_coeffs);
+registerProperty(camera_matrix);
+registerProperty(dist_coeffs);
     registerProperty(rectificaton_matrix);
     registerProperty(projection_matrix);
     registerProperty(rotation_matrix);
@@ -45,56 +46,67 @@ CameraInfoProvider::CameraInfoProvider(const std::string & name) :
 }
 
 CameraInfoProvider::~CameraInfoProvider() {
+
 }
 
 void CameraInfoProvider::prepareInterface() {
-	// Register data streams, events and event handlers HERE!
-	// Register data streams
-	registerStream("out_camera_info", &out_camerainfo);
-	registerStream("in_camera_info", &in_camerainfo);
+// Register data streams, events and event handlers HERE!
+// Register data streams
+registerStream("out_camera_info", &out_camerainfo);
+registerStream("in_camera_info", &in_camerainfo);
 
-	// Register handlers
+// Register handlers
     //"Generate data" handler
-	h_generate_data.setup(boost::bind(&CameraInfoProvider::generate_data, this));
-	registerHandler("generate_data", &h_generate_data);
-	addDependency("generate_data", NULL);
+h_generate_data.setup(boost::bind(&CameraInfoProvider::generate_data, this));
+registerHandler("generate_data", &h_generate_data);
+addDependency("generate_data", NULL);
 
     //"Reload file" handler
     h_reload_file.setup(boost::bind(&CameraInfoProvider::reload_file, this));
     registerHandler("reload_file", &h_reload_file);
+//    addDependency("reload_file", NULL);
 
     //"Update params" handler
-	h_update_params.setup(boost::bind(&CameraInfoProvider::update_params, this));
-	registerHandler("update_params", &h_update_params);
-	addDependency("update_params", &in_camerainfo);
+h_update_params.setup(boost::bind(&CameraInfoProvider::update_params, this));
+registerHandler("update_params", &h_update_params);
+addDependency("update_params", &in_camerainfo);
 
+CLOG(LNOTICE) << "CameraMatrix: " << camera_matrix;
 }
 
 bool CameraInfoProvider::onInit() {
-	return true;
+      if(data_file!="")
+      {
+	    CLOG(LINFO) <<"reload_file";
+	    reload_file();
+      }
+      else
+	CLOG(LINFO) <<"dont reload_file";
+      
+return true;
 }
 
 bool CameraInfoProvider::onFinish() {
-	return true;
+return true;
 }
 
 bool CameraInfoProvider::onStop() {
-	return true;
+return true;
 }
 
 bool CameraInfoProvider::onStart() {
-	return true;
+return true;
 }
 
 void CameraInfoProvider::generate_data() {
     LOG(LDEBUG) << "setWidth";
-	camera_info.setWidth(width);
+camera_info.setWidth(width);
     LOG(LDEBUG) << "setHeight";
-	camera_info.setHeight(height);
+camera_info.setHeight(height);
     LOG(LDEBUG) << "setCameraMatrix";
-	camera_info.setCameraMatrix(camera_matrix);
+camera_info.setCameraMatrix(camera_matrix);
     LOG(LDEBUG) << "setDistCoeffs";
-	camera_info.setDistCoeffs(dist_coeffs);
+camera_info.setDistCoeffs(dist_coeffs);
     LOG(LDEBUG) << "setRectificationMatrix";
     camera_info.setRectificationMatrix(rectificaton_matrix);
     LOG(LDEBUG) << "setProjectionMatrix";
@@ -104,15 +116,15 @@ void CameraInfoProvider::generate_data() {
     LOG(LDEBUG) << "setTranlationMatrix";
     camera_info.setTranlationMatrix(translation_matrix);
     LOG(LDEBUG) << "write";
-	out_camerainfo.write(camera_info);
+out_camerainfo.write(camera_info);
 }
 
 void CameraInfoProvider::update_params() {
-	Types::CameraInfo camera_info = in_camerainfo.read();
-	width = camera_info.width();
-	height = camera_info.height();
-	camera_matrix = camera_info.cameraMatrix();
-	dist_coeffs = camera_info.distCoeffs();
+Types::CameraInfo camera_info = in_camerainfo.read();
+width = camera_info.width();
+height = camera_info.height();
+camera_matrix = camera_info.cameraMatrix();
+dist_coeffs = camera_info.distCoeffs();
     rectificaton_matrix = camera_info.rectificationMatrix();
     projection_matrix = camera_info.projectionMatrix();
     rotation_matrix = camera_info.rotationMatrix();
